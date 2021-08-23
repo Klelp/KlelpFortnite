bd808153fafa0bb16f8c811f7fed86444047d2c8
Klelp <klelplgl70.am@gmail.com>
2021-08-23 23:56:27 +0300

    Create bot.py
    discord bot

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+import requests
+from discord.ext import commands
+from discord.ext import tasks
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def shop(ctx):
+    embed = discord.Embed(title="Магазин Предметов")
+    embed.set_image(url="https://fortool.fr/cm/assets/shop/ru.png")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def news (ctx):
+    r = requests.get("https://fortnite-api.com/v2/news/br")
+    data = r.json()
+    embed = discord.Embed(title="Новости")
+    embed.add_field(name="Date", value=data["data"]["date"])
+    embed.set_image(url=data["data"]["image"])
+    await ctx.send(embed=embed)
+
+bot.run("ODc5MTA2MjU4OTg3MjYyMDEz.YSK5gQ.7YYEFbHlTlI6HMMgzwQkNFCwq6c")
